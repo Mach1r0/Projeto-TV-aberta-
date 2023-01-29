2e7c88dc5144efd51c7c56f3360e6c50c59b4b95
Caio Braga <caio.braga.felipe@gmail.com>
2023-01-29 15:16:14 -0300

    Update ConnectionMVC.java

--- codigo/src/dao/ConnectionMVC.java
+++ codigo/src/dao/ConnectionMVC.java
@@ -14,7 +14,7 @@ public class ConnectionMVC {
 			e.printStackTrace();
 		}
 	try {
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/MVC?useSSL=false", "root", "2412Lulu.");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/MVC?useSSL=false", "root", "DAni2003#");
 		} catch(SQLException e) {
 			e.printStackTrace();
 		}
